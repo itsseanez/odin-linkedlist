d4c37363d68e2167ac7b86bff1d658ebfcef819c
Sean Ezenwa <seanezenwa@gmail.com>
2023-11-05 17:47:29 -0500

    Create append and prepend methods with head, tail, and size values

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,36 @@
 const LinkedList= (() => {
+    let head = null;
+    let tail= null;
+    let size = 0;
+
     
-})
+    const append = (value) => {
+        const newNode= Node(value)
+        size++
+        if (head === null) {
+            head= newNode;
+            tail= newNode;
+        } else {
+            let currentNode= head
+            while(currentNode.nextNode !== null) {
+                currentNode = currentNode.nextNode
+            }
+            currentNode.nextNode= newNode;
+            tail= newNode
+        }
+    };
+
+    const prepend = (value) => {
+        const newNode= Node(value)
+        size++
+        if (head === null) {
+            head= newNode;
+            tail= newNode;
+        } else {
+            newNode.nextNode= head
+            head= newNode
+        }
+    }
+
+    return{append, prepend}
+})();
